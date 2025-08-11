f14c14db84e1279f69646af12f411e5782701e81
Connor Sanders <connor@elastiflow.com>
2025-08-11 14:46:06 -0500

    check in

--- arrow-avro/src/schema.rs
+++ arrow-avro/src/schema.rs
@@ -198,8 +198,8 @@ pub struct Field<'a> {
     #[serde(borrow)]
     pub r#type: Schema<'a>,
     /// Optional default value for this field
-    #[serde(borrow, default)]
-    pub default: Option<&'a str>,
+    #[serde(default)]
+    pub default: Option<Value>,
 }
 
 /// An enumeration
